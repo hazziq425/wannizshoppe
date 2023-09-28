f7fa4f67c75fe91ee7414fe8058ba3bbd23ae81f
hazziq425 <146123539+hazziq425@users.noreply.github.com>
2023-09-28 15:07:21 +0800

    edit

--- user/checkout.php
+++ user/checkout.php
@@ -1,39 +1,62 @@
 <?php
 include('../connect/connection.php');
-
-// Check if a No ID parameter is passed in the URL
-if (isset($_GET['noID'])) {
-    $proid = $_GET['noID'];
-
-    // Retrieve product details from the database based on $proid
-    $viewproduct = "SELECT * FROM bajulelaki WHERE noID='$proid'";
-    $exeviewpro = $connect->query($viewproduct);
-    $data = $exeviewpro->fetch_assoc();
-
-    // Check if the product details were found
-    if ($data) {
-        $productName = $data['category'];
-        $size = $data['size'];
-        $quantity = $_SESSION['shopcart'][$proid];
-
-        // Display the selected product's details along with the checkout form
-        echo "<h1>Checkout</h1>";
-        echo "<p>No ID: $proid</p>";
-        echo "<p>Product Name: $productName</p>";
-        echo "<p>Size: $size</p>";
-        echo "<p>Quantity: $quantity</p>";
-        
-        // Add your checkout form fields here (Name, Email, Telephone Number)
-        echo "<form method='post' action='process_checkout.php'>";
-        echo "Name: <input type='text' name='name' required><br><br>";
-        echo "Email: <input type='email' name='email' required><br><br>";
-        echo "Telephone Number: <input type='tel' name='telno' required><br><br>";
-        echo "<input type='submit' name='submit' value='Complete Purchase'>";
-        echo "</form>";
-    } else {
-        echo "<p>Product not found.</p>";
-    }
-} else {
-    echo "<p>No No ID parameter specified.</p>";
-}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="your-css-file.css">
+    <div class="header">
+        <?php include 'header.php'; ?>
+    </div>
+</head>
+<body>
+    <?php
+    // Check if a No ID parameter is passed in the URL
+    if (isset($_GET['noID'])) {
+        $proid = $_GET['noID'];
+
+        // Retrieve product details from the database based on $proid
+        $viewproduct = "SELECT * FROM bajulelaki WHERE noID='$proid'";
+        $exeviewpro = $connect->query($viewproduct);
+        $data = $exeviewpro->fetch_assoc();
+
+        // Check if the product details were found
+        if ($data) {
+            $productName = $data['category'];
+            $size = $data['size'];
+            $quantity = $_SESSION['shopcart'][$proid];
+
+            // Display the selected product's details along with the checkout form
+            echo "<h1 class='checkout-heading'>Checkout</h1>";
+
+            echo "<table class='table'>";
+            echo "<tr><th>No ID</th><th>Product Name</th><th>Size</th><th>Quantity</th></tr>";
+            echo "<tr>";
+            echo "<td>$proid</td>";
+            echo "<td>$productName</td>";
+            echo "<td>$size</td>";
+            echo "<td>$quantity</td>";
+            echo "</tr>";
+            echo "</table>";
+
+            // Add your checkout form fields here (Name, Email, Telephone Number)
+            echo "<form method='post' action='checkoutprocess.php'>";
+            echo "<label for='name'>Name:</label>";
+            echo "<input type='text' id='name' name='name' class='input' required><br><br>";
+            echo "<label for='email'>Email:</label>";
+            echo "<input type='email' id='email' name='email' class='input' required><br><br>";
+            echo "<label for='telno'>Telephone Number:</label>";
+            echo "<input type='tel' id='telno' name='telno' class='input' required><br><br>";
+            echo "<input type='submit' name='submit' value='Complete Purchase' class='submit'>";
+            echo "</form>";
+
+        } else {
+            echo "<p class='error-message'>Product not found.</p>";
+        }
+        } else {
+            echo "<p class='error-message'>No No ID parameter specified.</p>";
+        }
+    ?>
+</body>
+</html>
